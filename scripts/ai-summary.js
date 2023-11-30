9b997a6698f05ee4da1a62c9bd87e9138e9054fc
qxdn <1464238196@qq.com>
2023-11-30 10:59:34 +0800

    Site updated: 2023-11-30 10:59:33

--- scripts/ai-summary.js
+++ scripts/ai-summary.js
@@ -5,4 +5,13 @@ new ChucklePostAI({
   key: "e3c69d80334be20b48d6",
   // 文章推荐方式，all：匹配数据库内所有文章进行推荐，web：仅当前站内的文章，默认all
   rec_method: "web",
+  hide_shuttle: true,
+  summary_directly: true,
+  pjax: true,
+  summary_speech: true,
+  interface: {
+    name: "AI摘要", // AI名称
+    introduce:
+      "我是文章辅助AI: 点击下方的按钮，让我生成本文简介、推荐相关文章等。", // 自我介绍
+  },
 });
